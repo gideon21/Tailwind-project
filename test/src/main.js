ea66f640e27e146e483a19d5c9e0b1d97fb86be9
gideon shobowale <goshobowale@uclan.ac.uk>
2021-12-07 20:08:31 +0000

    first commit

---
+++ test/src/main.js
@@ -0,0 +1,8 @@
+// When openBtn is clicked get the popup id in stylesheet and display block
+let openBtn = () => {
+    document.getElementById("modal").style.display = "block";
+};
+// When closeBtn is clicked get the popup id in stylesheet and display none
+let closeBtn = () => {
+    document.getElementById("modal").style.display = "none";
+};
